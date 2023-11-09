55ce8a87c194ee47262846d1e146f5a9fcb0252b
Osmer-sys <osmer95@hotmail.com>
2023-11-09 09:34:43 -0500

    ready for render.com

--- drfreactivos/settings.py
+++ drfreactivos/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,31 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i+mh(ponjc&l_wg$n0_1ah&azjm2gqah12aswfmkywp%0h33sv'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['localhost', 
-                 '127.0.0.1', 
-                 '[::1]',
-                 'localhost:8000/fabricante/',
-                 'localhost:8000/proveedor/',
-                 'localhost:8000/estado/',
-                 'localhost:8000/ubicacion/',
-                 'localhost:8000/rol/',
-                 'localhost:8000/area/',
-                 'localhost:8000/reactivo/',
-                 'localhost:8000/orden/',
-                 'localhost:8000/detalle/',
-                 'localhost:8000/admin/',
-                 'localhost:8000/login/',
-                 'localhost:8000/logout/',
-                 'localhost:8000/token/',
-                 'http://OSNA:8000/login/',
-                 'OSNA:8000',
-                 'osna'
-                 ]
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -64,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -97,14 +86,11 @@ WSGI_APPLICATION = 'drfreactivos.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_reactivos',
-        'USER': 'test',
-        'PASSWORD': 'BZvof03*',
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
@@ -143,6 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+    if not DEBUG:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+        STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
