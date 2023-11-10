ec2d164a53393e009a498815dedc9494ca531bf8
Osmer-sys <osmer95@hotmail.com>
2023-11-09 19:49:52 -0500

    se modifica el archivo build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
 
-# Verificar si el usuario ya existe
-if not User.objects.filter(username='admin').exists():
-    # Crear un nuevo superusuario
-    User.objects.create_superuser('superuser', 'superuser@mail.com', 's3*1416u')
-EOF
