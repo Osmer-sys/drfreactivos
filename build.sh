1b4494f2487bbb59c7bb992c6cb3591151f53c09
Osmer-sys <osmer95@hotmail.com>
2023-11-09 19:34:28 -0500

    se modifica el archivo build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --username superuser --email superuser@mail.com --password s3*1416u
+echo "s3*1416u"
